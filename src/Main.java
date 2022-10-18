b6ddc2901c8f72a99676b67aaaca70803ebe6e4d
Andrew Zola <and.zola.77@gmail.com>
2022-10-18 18:54:42 +0200

    Sum matrix

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Matrix matrix = new Matrix(3,2);
+
+        //System.out.println(matrix.getH());
+        //System.out.println(matrix.getW());
+        //System.out.println(Arrays.deepToString(matrix.getMatrix()));
+        //matrix.pprint();
+
+        Matrix m = new Matrix(new int[][]{{1,2,3},{4,5,6}});
+        //m.pprint();
+        //System.out.println(m.getH());
+        //System.out.println(m.getW());
+        matrix.sum(-4);
+        matrix.pprint();
+        m.sum(matrix);
+        m.pprint();
+
+
+    }
+}
